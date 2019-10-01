f04bc43d739cc8c3d264241c71f29db0f901f0c3
EdwinGomes <edwingomes@live.co.uk>
2019-10-01 23:28:09 +0100

    Create installagentchoclatey.ps1

---
+++ installagentchoclatey.ps1
@@ -0,0 +1,8 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install azure-pipelines-agent -y
+
+#Reboot
+Restart-Computer
